7a4f0aecdbd935f7837ab7db6b88c3a26b79a2e0
Brian Freeman <brian.freeman@rcoffee.io>
2021-06-19 18:22:20 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Append-only DB SDK
+
+Use this sdk to append to db and query results
---
+++ sdk.go
@@ -0,0 +1,56 @@
+package dbsdk
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+type AppendDbSDKClient struct {
+	stub DBServiceClient
+}
+
+// CreateLogger2Client creates a new sdk client
+// host is the hostname of the server
+// pathToCert is the path to the server's public certificate
+// port is the port number the server service is running on
+func CreateLogger2Client(host, pathToCert string, port int) *AppendDbSDKClient {
+	var sdk AppendDbSDKClient
+	creds, err := credentials.NewClientTLSFromFile(pathToCert, host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conn, err := grpc.Dial(fmt.Sprintf("%s:%d", host, port), grpc.WithTransportCredentials(creds), grpc.WithBlock(), grpc.WithTimeout(5*time.Second))
+	if err != nil {
+		log.Fatal(err)
+	}
+	sdk.stub = NewDBServiceClient(conn)
+	return &sdk
+}
+
+// Append will write a new row to the table
+func (s *AppendDbSDKClient) Append(table string, ts time.Time, dat []byte) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var tup DBTuple
+	tup.Ts = ts.UnixNano()
+	tup.Data = dat
+
+	_, err := s.stub.Append(ctx, &AppendRequest{Table: table, Data: &tup})
+	return err
+}
+
+// Query will return all the rows for a table that are between start and stop inclusive
+func (s *AppendDbSDKClient) Query(table string, start, stop time.Time) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := s.stub.Query(ctx, &QueryRequest{Table: table, Start: start.UnixNano(), Stop: stop.UnixNano()})
+	return err
+}
